0ab39b455da9f760dbbd5cb02e94e06c3900666f
Laboratorio-Info <kaffergabriel@gmail.com>
2026-02-27 22:18:44 -0300

    Retorno da página de cadastro a página de login

--- app/src/main/java/br/com/etecia/moveisapp/CadastrarActivity.java
+++ app/src/main/java/br/com/etecia/moveisapp/CadastrarActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.moveisapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CadastrarActivity extends AppCompatActivity {
 
+    ImageView iconReturnLoginCadas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.cadastrar_layout);
+
+        iconReturnLoginCadas = findViewById(R.id.iconReturnLoginCadas);
+
+        // Voltar para a tela de Login
+        iconReturnLoginCadas.setOnClickListener(v -> finish());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
