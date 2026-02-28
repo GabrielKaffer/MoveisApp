939ca26939c526ec04ba338971373c1549cf550a
Laboratorio-Info <kaffergabriel@gmail.com>
2026-02-27 21:49:48 -0300

    Atualização do layout da tela de cadastro

--- app/src/main/java/br/com/etecia/moveisapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/moveisapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.moveisapp;
 
+import android.content.Intent;   // 👈 IMPORTANTE
 import android.os.Bundle;
+import android.view.View;        // 👈 IMPORTANTE
+import android.widget.Button;    // 👈 IMPORTANTE
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_layout);
+
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CadastrarActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
